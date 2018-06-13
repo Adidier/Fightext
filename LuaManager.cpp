5a9cb4397ab18a0ebfb2d1ce6a5ca877be6fb527
Adidier <adidier.perez@gmail.com>
2018-06-13 08:31:04 -0500

    read lua

--- LuaManager.cpp
+++ LuaManager.cpp
@@ -2,21 +2,48 @@
 
 vector<Move> LuaManager::moves;
 
-void LuaManager::SetDefence(lua_State *L)
+int LuaManager::SetDefence(lua_State *L)
 {
 	Move m;
+	const char* str = lua_tostring(L, 1);
+	const int value = (int)lua_tonumber(L, 2);
+	m.name = str;
+	m.type = 2;
+	m.value = value;
 	moves.push_back(m);
+	return 0;
 }
 
-void LuaManager::SetAttack(lua_State *L)
+int LuaManager::SetAttack(lua_State *L)
 {
 	Move m;
+	const char* str = lua_tostring(L, 1);
+	const int value = (int)lua_tonumber(L, 2);
+	m.name = str;
+	m.type = 1;
+	m.value = value;
 	moves.push_back(m);
+	return 0;
 }
 
 vector<Move> LuaManager::LoadPlayer(string nameFile)
 {
 	moves.clear();
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	luaopen_base(L);
+	luaopen_string(L);
+	luaopen_math(L);
+
+	lua_pushcfunction(L, SetDefence);
+	lua_setglobal(L, "write");
+	lua_pushcfunction(L, SetAttack);
+	lua_setglobal(L, "write");
+
+	if (luaL_loadfile(L, nameFile.c_str()) || lua_pcall(L, 0, 0, 0))
+	{
+		printf("cannot run configuration file: %s", lua_tostring(L, -1));
+	}
 
 	return moves;
 }
